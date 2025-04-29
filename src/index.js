a4974424965cb0a9520461d650016087a21e06f7
Tarik791 <terzotarik@gmail.com>
2025-04-29 18:46:15 +0200

    test

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import express from 'express';
-import cors from 'cors';
 import clientRoutes from './routes/clientRoute.js'
 import path from 'path';
 import { fileURLToPath } from 'url';
@@ -9,17 +8,12 @@ env.config();
 
 const app = express();
 const port = process.env.PORT || 3001;
-const corsOptions ={
-    origin:'https://thiiqqa.com', 
-    credentials:true,            
-    optionSuccessStatus:200
-}
+
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 
-app.use(cors(corsOptions));
 app.use(express.json())  
 
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
