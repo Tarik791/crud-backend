ca9fb42a79804d719d1cc67efb78b23d9ba3f218
Tarik791 <terzotarik@gmail.com>
2025-04-29 13:10:09 +0200

    init 4

--- src/db.js
+++ src/db.js
@@ -3,14 +3,20 @@ import env from 'dotenv';
 
 env.config();
 
-const db = await mysql.createConnection({
+const pool = mysql.createPool({
     host: process.env.DB_HOST || "localhost",
     user: process.env.DB_USER || "root",
     password: process.env.DB_PASSWORD || "password",
     database: process.env.DB_DATABASE || "client_db",
+    port: process.env.DB_PORT || 3306,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0,
+    enableKeepAlive: true,
+    keepAliveInitialDelay: 10000
 });
 
 export const query = async (sql, params) => {
-    const [rows] = await db.execute(sql, params);  
+    const [rows] = await pool.execute(sql, params);
     return rows;
 };
--- src/index.js
+++ src/index.js
@@ -9,17 +9,12 @@ env.config();
 
 const app = express();
 const port = process.env.PORT || 3001;
-const corsOptions ={
-    origin:'https://thiiqqa.com/', 
-    credentials:true,            
-    optionSuccessStatus:200
-}
+
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 
-app.use(cors(corsOptions));
 app.use(express.json())  
 
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
