4cf09aa0469b6f10ac29852f07c3ce06435f28d3
Tarik791 <terzotarik@gmail.com>
2025-04-29 15:39:05 +0200

    new

--- src/index.js
+++ src/index.js
@@ -14,21 +14,11 @@ const port = process.env.PORT || 3001;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const sslOptions = {
-    key: fs.readFileSync('/etc/letsencrypt/live/srv806430.hstgr.cloud/privkey.pem'),
-    cert: fs.readFileSync('/etc/letsencrypt/live/srv806430.hstgr.cloud/fullchain.pem')
-  };
-
-const corsOptions = {
-    origin: ['https://thiiqqa.com', 'http://localhost:3000'],
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-};
-app.use(cors(corsOptions));
 app.use(express.json())  
 
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.use('/api', clientRoutes)
 
-https.createServer(sslOptions, app).app.listen(port, () => {
+app.listen(port, () => {
     console.log("listening on port 3001")
 })
