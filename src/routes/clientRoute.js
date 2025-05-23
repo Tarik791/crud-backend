9cfd30151a4197e4520b5b0300155748caf0b7f5
Tarik791 <terzotarik@gmail.com>
2025-05-23 17:38:40 +0200

    init

--- src/routes/clientRoute.js
+++ src/routes/clientRoute.js
@@ -19,29 +19,29 @@ router.post("/messages/read", messageReadController.updateMessageRead);
 
 
 router.get("/cars", carController.getCars);
-router.post("/cars", upload.single("image"), verifyToken, carController.createCar);
-router.put("/cars/:id", verifyToken, carController.updateCar);
-router.delete("/cars/:id", verifyToken, carController.deleteCar);
+router.post("/cars", upload.single("image"), carController.createCar);
+router.put("/cars/:id", carController.updateCar);
+router.delete("/cars/:id", carController.deleteCar);
 router.get("/cars/search", carController.searchCars);
 
-router.post("/messages", verifyToken, messagesController.createMessage);
+router.post("/messages", messagesController.createMessage);
 router.get("/messages/:id", messagesController.getMessagesByReservationId);
-router.put("/messages/:id", verifyToken, messagesController.updateMessage);
-router.delete("/messages/:id", verifyToken, messagesController.deleteMessage);
+router.put("/messages/:id", messagesController.updateMessage);
+router.delete("/messages/:id", messagesController.deleteMessage);
 // clientRoute.js
 router.get("/messages/:id/paginated", messagesController.getMessagesByReservationIdPaginated);
 
 
 router.get("/reservations", reservationController.getReservations);
 router.post("/reservations", upload.single("document"), reservationController.createReservation);
-router.put("/reservations/:id", verifyToken, reservationController.updateReservation);
-router.delete("/reservations/:id", verifyToken, reservationController.deleteReservation);
+router.put("/reservations/:id", reservationController.updateReservation);
+router.delete("/reservations/:id", reservationController.deleteReservation);
 router.get("/reservations/search", reservationController.searchReservations);
 
 router.get("/users", userController.getUsers);
-router.post("/users", upload.single("image"), verifyToken, userController.createUsers);
-router.put("/users/:id", upload.single("image"), verifyToken, userController.updateUsers);
-router.delete("/users/:id", verifyToken, userController.deleteUser);
+router.post("/users", upload.single("image"), userController.createUsers);
+router.put("/users/:id", upload.single("image"), userController.updateUsers);
+router.delete("/users/:id", userController.deleteUser);
 router.get("/users/search", userController.searchUsers);
 // router.post('/testimonials', userController.createTestimonials);
 router.post('/testimonials', userController.createTestimonialsUsersComments);
@@ -49,9 +49,9 @@ router.get("/testimonials", userController.getTestimonials);
 //router.post('/testimonials/:testimonialId/comment', userController.createTestimonialsComment);
 
 router.get("/clients", clientController.getClients);
-router.post("/clients", upload.single("image"), verifyToken, clientController.createClients);
-router.put("/clients/:id", verifyToken, clientController.updateClients);
-router.delete("/clients/:id", verifyToken, clientController.deleteClient);
+router.post("/clients", upload.single("image"), clientController.createClients);
+router.put("/clients/:id", clientController.updateClients);
+router.delete("/clients/:id", clientController.deleteClient);
 router.get("/clients/search", clientController.searchClients);
 
 router.post("/auth/login", authController.loginUser);
