96cb48f21a5b62ac5cc3223275f009ae331ae409
Tarik791 <terzotarik@gmail.com>
2025-05-23 17:44:02 +0200

    remove

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import clientRoutes from './routes/clientRoute.js'
 import path from 'path';
 import { fileURLToPath } from 'url';
 import env from "dotenv";
-import cors from 'cors';
 
 env.config(); 
 
@@ -13,11 +12,6 @@ const port = process.env.PORT || 3001;
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-
-app.use(cors({
-  origin: 'http://localhost:3000',
-  credentials: true
-}));
 
 app.use(express.json())  
 
