c0781a740a7784e63f7fc151bc29d1f46e4d68e7
Tarik791 <terzotarik@gmail.com>
2025-05-26 17:57:07 +0200

    test

--- src/index.js
+++ src/index.js
@@ -14,8 +14,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 app.use(express.json({ limit: '50mb' }));
-app.use(express.urlencoded({ limit: '50mb', extended: true }));
-
+app.use(express.urlencoded({limit: '50mb'}));
 
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.use('/api', clientRoutes)
