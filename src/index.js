e71d6940576c2e47f5c93ce2ce81bbc62084cd0c
Tarik791 <terzotarik@gmail.com>
2025-05-26 17:55:01 +0200

    test

--- src/index.js
+++ src/index.js
@@ -18,8 +18,9 @@ app.use(cors({
   origin: 'https://thiiqqa.com',
   credentials: true
 }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
-app.use(express.json())  
 
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.use('/api', clientRoutes)
