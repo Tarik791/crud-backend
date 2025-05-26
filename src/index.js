9fa5473f089c9f9c38ea29a6d73dd532825ec7f0
Tarik791 <terzotarik@gmail.com>
2025-05-27 00:11:34 +0200

    init

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import clientRoutes from './routes/clientRoute.js'
 import path from 'path';
 import { fileURLToPath } from 'url';
 import env from "dotenv";
-import cors from 'cors';
+
 env.config(); 
 
 const app = express();
@@ -12,28 +12,6 @@ const port = process.env.PORT || 3001;
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-var allowlist = ['https://thiiqqa.com', 'https://thiiqqa.com/', 'http://localhost:3000']
-var corsOptionsDelegate = function (req, callback) {
-  var corsOptions;
-  if (allowlist.indexOf(req.header('Origin')) !== -1) {
-    corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
-  } else {
-    corsOptions = { origin: false } // disable CORS for this request
-  }
-  callback(null, corsOptions) // callback expects two parameters: error and options
-}
-
-app.use((req, res, next) => {
-    if (req.method === 'OPTIONS') {
-        res.header('Access-Control-Allow-Origin', req.headers.origin);
-        res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-        res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-        res.header('Access-Control-Allow-Credentials', 'true');
-        return res.status(200).json({});
-    }
-    next();
-});
-
 
 app.use(express.json())  
 
