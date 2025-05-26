03b7b7dc5a06fd6dfd2c0d6bd57af841e2cc995f
Tarik791 <terzotarik@gmail.com>
2025-05-27 00:20:27 +0200

    test

--- src/index.js
+++ src/index.js
@@ -1,32 +1,29 @@
 import express from 'express';
-import clientRoutes from './routes/clientRoute.js'
+import clientRoutes from './routes/clientRoute.js';
 import path from 'path';
 import { fileURLToPath } from 'url';
-import env from "dotenv";
+import env from 'dotenv';
+import cors from 'cors'; // ✅ Use import instead of require
 
-var cors = require('cors')
+env.config();
 
+const app = express();
+const port = process.env.PORT || 3001;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Middlewares
 app.use(cors({
   origin: 'https://thiiqqa.com',
   credentials: true
 }));
 
-env.config(); 
-
-const app = express();
-const port = process.env.PORT || 3001;
-
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-
-
-app.use(express.json())  
-
+app.use(express.json());
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
-app.use('/api', clientRoutes)
+app.use('/api', clientRoutes);
 
+// Start server
 app.listen(port, () => {
-    console.log("listening on port 3001")
-})
+  console.log(`listening on port ${port}`);
+});
