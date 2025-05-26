2f2228454cc5d0867d5ab3602047e8c7f74e08f1
Tarik791 <terzotarik@gmail.com>
2025-05-26 19:06:28 +0200

    test

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import clientRoutes from './routes/clientRoute.js'
 import path from 'path';
 import { fileURLToPath } from 'url';
 import env from "dotenv";
-
+import cors from 'cors';
 env.config(); 
 
 const app = express();
@@ -12,7 +12,12 @@ const port = process.env.PORT || 3001;
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-
+app.use(cors({
+    origin: 'https://thiiqqa.com', // ili '*' za sve domene
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true // ako koristiš cookies ili auth
+}));
 app.use(express.json())  
 
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
