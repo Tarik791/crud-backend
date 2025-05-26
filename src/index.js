99ac04d227e51be8e141bf3198707c46404b2527
Tarik791 <terzotarik@gmail.com>
2025-05-26 17:59:06 +0200

    test

--- src/index.js
+++ src/index.js
@@ -13,8 +13,8 @@ const port = process.env.PORT || 3001;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-app.use(express.json({ limit: '50mb' }));
-app.use(express.urlencoded({limit: '50mb'}));
+app.use(express.json({limit: "10mb", extended: true}))
+app.use(express.urlencoded({limit: "10mb", extended: true, parameterLimit: 50000}))
 
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.use('/api', clientRoutes)
